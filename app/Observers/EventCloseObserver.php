4902b22f18dc5ae9ad69cc6809d98821dd3f3641
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2024-06-28 20:45:42 +0700

    fix: local datetime event observer

--- app/Observers/EventCloseObserver.php
+++ app/Observers/EventCloseObserver.php
@@ -9,6 +9,7 @@ class EventCloseObserver
 {
     private function closeEventIfNeeded(Event $event)
     {
+        date_default_timezone_set('Asia/Jakarta');
         $now = Carbon::now();
 
         if (!$event->is_close && $event->hari_tanggal <= $now->toDateString() && $event->waktu_mulai <= $now->toTimeString()) {
@@ -17,26 +18,41 @@ class EventCloseObserver
         }
     }
 
+    /**
+     * Handle the Event "retrieved" event.
+     */
     public function retrieved(Event $event): void
     {
         $this->closeEventIfNeeded($event);
     }
 
+    /**
+     * Handle the Event "updated" event.
+     */
     public function updated(Event $event): void
     {
         $this->closeEventIfNeeded($event);
     }
 
+    /**
+     * Handle the Event "deleted" event.
+     */
     public function deleted(Event $event): void
     {
         $this->closeEventIfNeeded($event);
     }
 
+    /**
+     * Handle the Event "restored" event.
+     */
     public function restored(Event $event): void
     {
         $this->closeEventIfNeeded($event);
     }
 
+    /**
+     * Handle the Event "force deleted" event.
+     */
     public function forceDeleted(Event $event): void
     {
         $this->closeEventIfNeeded($event);
