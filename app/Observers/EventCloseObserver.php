00dc3510cdc7ed71f97fefe8907fb63887246564
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2024-07-01 20:54:37 +0700

    update: retrive only in event observer

--- app/Observers/EventCloseObserver.php
+++ app/Observers/EventCloseObserver.php
@@ -25,36 +25,4 @@ class EventCloseObserver
     {
         $this->closeEventIfNeeded($event);
     }
-
-    /**
-     * Handle the Event "updated" event.
-     */
-    public function updated(Event $event): void
-    {
-        $this->closeEventIfNeeded($event);
-    }
-
-    /**
-     * Handle the Event "deleted" event.
-     */
-    public function deleted(Event $event): void
-    {
-        $this->closeEventIfNeeded($event);
-    }
-
-    /**
-     * Handle the Event "restored" event.
-     */
-    public function restored(Event $event): void
-    {
-        $this->closeEventIfNeeded($event);
-    }
-
-    /**
-     * Handle the Event "force deleted" event.
-     */
-    public function forceDeleted(Event $event): void
-    {
-        $this->closeEventIfNeeded($event);
-    }
 }
